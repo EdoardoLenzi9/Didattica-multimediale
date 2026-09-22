a6a6fddd76d5a3ed46632e075ac00e1b38a56d75
edoardolenzi9@gmail.com <edoardolenzi9@gmail.com>
2026-09-22 07:56:49 +0200

    wip

---
+++ 2LSAB_Informatica/000.py
@@ -0,0 +1,33 @@
+# ------------------------------------------------------------------------#
+#                                 VARIABILI                               #
+# ------------------------------------------------------------------------#
+
+# dichiara una variabile "x" di tipo intero ("int") con valore 314
+
+# dichiara una variabile "y" di tipo numerico con virgola ("float") con valore 3.14
+
+# dichiara una variabile "z" di tipo booleano ("bool") con valore True
+
+# dichiara una variabile "w" di tipo testo/stringa ("str") con valore "Hello World!"
+
+# stampa il valore di x, y, z e w
+
+# ------------------------------------------------------------------------#
+#                           RIASSEGNA UNA VARIABILE                       #
+# ------------------------------------------------------------------------#
+
+# dichiara una variabile "a" di tipo intero
+
+# assegna ad a il valore 1
+
+# stampa il valore di a
+
+# riassegna ad a il valore 2
+
+# stampa il valore di a
+
+# riassegna ad a il valore 3
+
+# stampa il valore di a
+
+# stampa il valore iniziale di a
