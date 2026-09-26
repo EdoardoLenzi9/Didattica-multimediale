b7c2f450f8748c43d650d303537b7b0ee0bf92a9
edoardolenzi9@gmail.com <edoardolenzi9@gmail.com>
2026-09-26 16:21:06 +0200

    wip

---
+++ 2LSAB_Informatica/008.py
@@ -0,0 +1,14 @@
+# ------------------------------------------------------------------------#
+#                                     INPUT                               #
+# ------------------------------------------------------------------------#
+
+# chiedi all'utente in input la sua eta e salvala nella variabile intera x
+# usando la funzione built-in "input" 
+
+# ------------------------------------------------------------------------#
+#                    IF - CAPITOLO 1 LIBRO (pag 2 - 19)                   #
+# ------------------------------------------------------------------------#
+
+# se la variabile x >= 18 stampa "sei maggiorenne"
+# se la variabile x <  18 stampa "sei minorenne"
+# se la variabile x >  50 stampa "sei over cinquanta"
---
+++ 2LSAB_Informatica/009.py
@@ -0,0 +1,19 @@
+# ------------------------------------------------------------------------#
+#                    FOR - CAPITOLO 1 LIBRO (pag 2 - 19)                  #
+# ------------------------------------------------------------------------#
+
+# stampa i numeri da 1 a 100 usando la funzione range e il ciclo for
+
+# stampa i numeri da 10 a 100 usando la funzione range e il ciclo for
+
+# stampa i numeri pari da 1 a 100
+
+# ------------------------------------------------------------------------#
+#                                     WHILE                               #
+# ------------------------------------------------------------------------#
+
+# stampa i numeri da 1 a 100 usando il ciclo WHILE
+
+# stampa i numeri da 10 a 100 usando il ciclo WHILE
+
+# stampa i numeri pari da 1 a 100
